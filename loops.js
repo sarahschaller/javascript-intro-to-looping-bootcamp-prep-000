ee5f354148361e839bb00d489d4963f5356df58e
Sarah Schaller <sarcsch@aol.com>
2017-05-20 14:37:28 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+var array = [];
+
+var forLoop = function(array) {
+  for (var i = 0; i < 25; i++) {
+    if(i === 0) {
+      array.push('I am 1 strange loop.')
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+};
+
+var whileLoop = function(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done';
+};
+
+var doWhileLoop = function(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
